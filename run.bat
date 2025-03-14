3613ac39e9b04c45ccfb93ff06528f6b21a0ac61
marioatmajanugraha <mario.vodrec@gmail.com>
2025-03-14 21:29:06 +0700

    first commit

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+title Monster Kombat Automation
+echo Memulai bot...
+node index.obf.js
+echo Bot telah berhenti.
+timeout /t 3 >nul
+echo Tekan tombol apa saja untuk keluar...
+pause >nul
